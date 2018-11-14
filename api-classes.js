a66dc4ddea89ac8576e906f717a1140f381c998d
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-11-14 14:51:45 -0800

    added getStories method

--- api-classes.js
+++ api-classes.js
@@ -1,12 +1,42 @@
-const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+const BASE_URL = 'https://hack-or-snooze-v2.herokuapp.com';
 
 class StoryList {
+  constructor(stories) {
+    this.stories = stories;
+  }
   static getStories(cb) {
     // fetch stories from API
-    return cb(/* fetched stories go here */);
+    let settings = {
+      async: true,
+      crossDomain: true,
+      url: `${BASE_URL}/stories`,
+      method: 'GET',
+      headers: {}
+    };
+    $.ajax(settings).done(function(response) {
+      let storyList = new StoryList(response);
+      return cb(storyList);
+    });
   }
 }
 
-class User {}
+class User {
+  constructor(username, password, name, loginToken, favorites, ownStories) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.loginToken = loginToken;
+    this.favorites = favorites;
+    this.ownStories = ownStories;
+  }
+}
 
-class Story {}
+class Story {
+  constructor(author, title, url, username, storyID) {
+    this.author = author;
+    this.title = title;
+    this.url = url;
+    this.username = username;
+    this.storyID = storyID;
+  }
+}
