84d48e234af34140110cc80e992943d8c65fb834
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-11-15 22:23:46 -0800

    Added layout for the remainder of the features that need to be added.

--- hacksnooze.js
+++ hacksnooze.js
@@ -17,6 +17,19 @@ $(function() {
   const $logout = $('#logout');
   let localUser;
 
+  // BONUS STUFF
+
+  // TO DO: Create a user profile page or window or something.
+  // Not sure how we should handle this yet, seperate page or just a dropdown menu
+  // User profile should contain the username, name, and a censored password field.
+  // Update changes to server with update user method.
+  // Functionality to change these entries can be added in the form of a form / button.
+
+  // TO DO: Editing Stories
+  // Stories can be edited by clicking some sort of edit button, likely built using a similar function to the delete event listener
+  // On clicking the edit button for a specific story, a dropdown menu will appear allowing for modification of title, url, link, etc etc
+  // Call the update story method from the api to update changes to the server.
+
   $submit.on('click', function() {
     $newForm.slideToggle();
   });
@@ -31,7 +44,8 @@ $(function() {
     // Find all attributes with data-username = localuser's username
     // Show trash can icon to all valid matches.
     // On second click of delete
-    // Find all "clicked" trash cans and delete them from dom, and call remove story on their id, using their dom id.
+    // Find all "clicked" trash cans and delete them from dom,
+    // and call remove story on their id, using their id stored in the attr "id"
     // Hide all trash cans again.
   });
 
@@ -78,6 +92,10 @@ $(function() {
 
   // On submitting a new story, add new story to the homepage
   // NEEDS: ADD SERVER INTERACTION
+  // SHOULD CALL storyList.addStory(localUser, OBJ, cb)
+  // WHERE OBJ is an object literal with keys { title = $title.val(), url = $url.val, author = localUser.username }
+  // CB is a callback with one parameter 'response', response is an array containing all stories.
+  // Not sure if the callback needs to do anything
   $newForm.on('submit', function(e) {
     e.preventDefault();
 
@@ -107,6 +125,16 @@ $(function() {
   });
 
   // on clicking the star, add item to favorites and switch the icon
+  // NEEDS: ADD SERVER INTERACTION (ADD TO USER FAVORITES/REMOVE FROM USER FAVORITES)
+  // find the story's id from the id attribute on the dom element
+  // Adds the story id to a favorite object or array stored in local storage
+  //
+  // UNDER THE StoryList.getStories function below:
+  // Call the user.favorite array and
+  // check the user's favorite array for any story on the page,
+  // if the story ids match, then change the star to be filled in.
+  // Note: to save on time efficiency, convert the favorites array to an object with keys being the story ids, then search by key
+  // In this case, value doesnt matter, the key needs to simply exist.
   $stories.on('click', '.far, .fas', function(e) {
     $(e.target).toggleClass('far fas');
   });
